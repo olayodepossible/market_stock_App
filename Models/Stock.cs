e8a41394240688c44e455834e66d372f8ab3e390
Akin <olayodepossible@gmail.com>
2025-01-20 19:26:52 +0100

    implement stock model

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,15 @@ namespace stock_app.Models
     {
         public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDividend { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
