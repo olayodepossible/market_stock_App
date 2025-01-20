4e0f5af3bd8d40dcf0284516db234a1bfea292ae
Akin <olayodepossible@gmail.com>
2025-01-20 19:34:47 +0100

    implement comment model

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -7,6 +7,14 @@ namespace stock_app.Models
 {
     public class Comment
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public int? StockId { get; set; }
+
+        //Navigation property
+        public Stock? Stock { get; set; }
     }
 }
